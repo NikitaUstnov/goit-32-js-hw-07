eff550c7bb923d78414ea3a7b907f424632fdb85
Nikita Ustinov <65974693+NikitaUstnov@users.noreply.github.com>
2021-05-11 23:51:17 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,7 @@
-const categoriesList = document.querySelector("#categories");
-const categoriesListItem = document.querySelectorAll(".item");
-
-categoriesListItem.forEach((el) => {
-  console.log(
-    `Категрия: ${el.children[0].textContent}\nКоличество элементов: ${el.children[1].childElementCount}`
+const categoriesListItem = document
+  .querySelectorAll(".item")
+  .forEach((el) =>
+    console.log(
+      `Категрия: ${el.children[0].textContent}\nКоличество элементов: ${el.children[1].childElementCount}`
+    )
   );
-});
